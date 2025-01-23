a430c4c88f3543e0974346fa279ac94cd5b62b9f
derek-dewald <121310981+derek-dewald@users.noreply.github.com>
2025-01-23 13:28:33 -0800

    Update dashboard.py

--- Streamlit/DataDictionary/dashboard.py
+++ Streamlit/DataDictionary/dashboard.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import plotly.express as px
 
+st.set_page_config(
+    page_title="Data Dictionary App",
+    page_icon="📖",
+    layout="wide"
+)
+
 # Streamlit app layout
 st.title("Streamlit App")
 
