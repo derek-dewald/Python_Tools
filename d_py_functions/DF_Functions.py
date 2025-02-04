241d9a8048c5c6610e9f08493a3f6dfb8170c5fb
Derek Dewald <derekdewald@Dereks-MacBook-Pro.local>
2025-02-04 15:50:18 -0800

    update

--- d_py_functions/DF_Functions.py
+++ d_py_functions/DF_Functions.py
@@ -23,3 +23,15 @@ def BinaryColumnCreator(df,
           df[new_column_name] = np.where((df[key]==value)&(df[new_column_name]==1),1,0)
         except:
           df[new_column_name] = np.where(df[key]==value,1,0)
+
+
+def TextClean(df, 
+              column_list, 
+              clean_type='only_digits'):
+
+    if clean_type == 'only_digits':
+        for column in column_list:
+            df[column] = df[column].astype(str).str.replace(r'[^\d.]', "", regex=True)
+            df[column] = pd.to_numeric(df[column], errors='coerce')
+
+    return df
