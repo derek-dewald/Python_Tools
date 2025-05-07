43d4b20778832e3bf563d8a9b5ac66a85de1d3cc
Derek Dewald <derekdewald@Dereks-MacBook-Pro.local>
2025-05-06 23:52:05 -0700

    update

--- d_py_functions/MLPipeline.py
+++ d_py_functions/MLPipeline.py
@@ -58,10 +58,41 @@ def ClassificationMetrics(df,
     return df,pd.DataFrame([results_dict.values()],columns=results_dict.keys())
 
 
+def apply_scaling(X_train, X_test, scaler=None):
+    """
+    Applies optional scaling to training and test datasets.
+    
+    Parameters:
+        X_train (np.array or pd.DataFrame): Training features.
+        X_test (np.array or pd.DataFrame): Test features.
+        scaler (str or None): Type of scaling to apply. 
+                              Options: 'standard', 'normalization', or None.
+    
+    Returns:
+        Scaled X_train and X_test.
+    """
+    scalers = {
+        'standard': StandardScaler(),
+        'normal': MinMaxScaler()
+    }
+
+    if scaler in scalers:
+        scaler_instance = scalers[scaler]
+        X_train = scaler_instance.fit_transform(X_train)
+        X_test = scaler_instance.transform(X_test)
+
+    return X_train, X_test, scaler_instance
+
 
 def SKLearnModelList(regressor_type=None):
     '''
     Function to generate a list of all scikit-learn estimators with type classification.
+    
+    Imports List from all_estimators (sklearn.utils) 
+    Imports D-Data Dashboard and Merges Column Dataset Size, to help filter for model processes.
+
+    Parameter:
+        Regressor_type: returns a list of relevant regressors if a specific regressor type is Picked. Types include: 
     
     Returns:
         df_final (pd.DataFrame): DataFrame of all estimators with class path, type labels, and module split.
@@ -72,6 +103,10 @@ def SKLearnModelList(regressor_type=None):
     
     # 2. Convert to DataFrame
     df = pd.DataFrame(estimators, columns=['Model Name', 'Estimator Class'])
+
+    # Merge In Dataset Size to prevent running of REALLY slow models.
+    temp = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQq1-3cTas8DCWBa2NKYhVFXpl8kLaFDohg0zMfNTAU_Fiw6aIFLWfA5zRem4eSaGPa7UiQvkz05loW/pub?output=csv')[['Word','Dataset Size']].rename(columns={'Word':'Model Name'})
+    df = df.merge(temp,on='Model Name',how='left')
     
     # 3. Extract full class path
     df['Full Class Path'] = df['Estimator Class'].astype(str).str.replace("<class '", "", regex=False).str.replace("'>", "", regex=False)
@@ -105,28 +140,3 @@ def SKLearnModelList(regressor_type=None):
         return df_final
     else:
         return df_final[df_final['Estimator Type'].str.contains(regressor_type)]
-
-def apply_scaling(X_train, X_test, scaler=None):
-    """
-    Applies optional scaling to training and test datasets.
-    
-    Parameters:
-        X_train (np.array or pd.DataFrame): Training features.
-        X_test (np.array or pd.DataFrame): Test features.
-        scaler (str or None): Type of scaling to apply. 
-                              Options: 'standard', 'normalization', or None.
-    
-    Returns:
-        Scaled X_train and X_test.
-    """
-    scalers = {
-        'standard': StandardScaler(),
-        'normal': MinMaxScaler()
-    }
-
-    if scaler in scalers:
-        scaler_instance = scalers[scaler]
-        X_train = scaler_instance.fit_transform(X_train)
-        X_test = scaler_instance.transform(X_test)
-
-    return X_train, X_test, scaler_instance
