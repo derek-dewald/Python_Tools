78141a8938bfaea729c8da572a712c22377f4430
Derek Dewald <derekdewald@berkeley.edu>
2026-01-14 15:39:46 -0800

    automatic update 2026-01-14 15:39:46

--- automated_py_jobs/daily_job.py
+++ automated_py_jobs/daily_job.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append("/Users/derekdewald/Documents/Python/Github_Repo/d_py_functions")
 
 from daily_processes import generate_dictionary,create_py_table_dict,parse_dot_py_folder
-from connections import DownloadFilesFromGit,BackUpGoogleSheets
+from connections import download_file_from_git,backup_google_worksheets
 
 
 # Updates saved //Github_Repo//Data//daily_test_results.csv which is Streamlit Input, Via Git.
